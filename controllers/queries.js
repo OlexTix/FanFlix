8869b239b649b0156c2da9e0feb5f7df1e15a4e9
DiFortis <difortis3@gmail.com>
2023-04-21 11:56:41 +0200

    finished users endpoints for new database

--- controllers/queries.js
+++ controllers/queries.js
@@ -5,80 +5,84 @@ const config = require("../config/auth.config");
 const { oleCheckJWT } = require("../middleware");
 
 const Pool = require('pg').Pool
+
+const connectionString = process.env.DATABASE_LINK;
+
 const poolDB = new Pool({
-  user: process.env.DATABASE_USER_NAME,
-  host: process.env.DATABASE_HOST,
-  database: process.env.DATABASE_NAME,
-  password: process.env.DATABASE_PASSWORD,
-  port: process.env.DATABASE_PORT,
-})
+  connectionString,
+});
 
-const getUsers = (req, res) => {
-  poolDB.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
-    if (error) {
-      throw error
+const getUsers = async (req, res) => {
+  const client = await poolDB.connect();
+
+  try {
+    const { rows } = await client.query('SELECT id_user, first_name, last_name, email, phone, birth_date, role, registration_date, is_active FROM "User" ORDER BY "id_user" ASC');
+    res.status(200).json(rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: err.message });
+  } finally {
+    client.release();
+  }
+};
+
+const getUserById = async (req, res) => {
+  const id = parseInt(req.params.id);
+  const client = await poolDB.connect();
+
+  try {
+    const {
+      rows: [user],
+    } = await client.query('SELECT id_user, first_name, last_name, email, phone, birth_date, role, registration_date, is_active FROM "User" WHERE "id_user" = $1', [id]);
+
+    if (!user) {
+      res.status(404).json({ message: 'User not found' });
+    } else {
+      res.status(200).json(user);
     }
-    res.status(200).json(results.rows)
-  })
-}
-
-const getUserById = (req, res) => {
-  const id = parseInt(req.params.id)
-
-  poolDB.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
-    if (error) {
-      throw error
-    }
-    res.status(200).json(results.rows)
-  })
-}
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: err.message });
+  } finally {
+    client.release();
+  }
+};
 
 const registerUser = async (req, res) => {
-  const { name, email, password, roles } = req.body;
+  const { first_name, last_name, email, password, phone, birth_date, role } = req.body;
 
-  // Default to "user" role if none provided
-  const userRoles = roles || ["klient"];
+  // Default to "client" role if none provided or an invalid role was given
+  const validRoles = ['client', 'employee', 'admin'];
+  const userRole = validRoles.includes(role) ? role : 'client';
 
   try {
     const client = await poolDB.connect();
 
-    // Check if user with same username or email already exists
+    // Check if user with same email already exists
     const {
       rows: existingUsers,
     } = await client.query(
-      "SELECT * FROM users WHERE name=$1 OR email=$2",
-      [name, email]
+      'SELECT * FROM "User" WHERE email=$1',
+      [email]
     );
     if (existingUsers.length > 0) {
-      res.status(400).send({ message: "Username or email is already taken" });
+      res.status(400).send({ message: "Email is already taken" });
       return;
     }
 
-    // Check if provided roles exist in user_roles table
+    // Insert new user with hashed password and default role
     const {
-      rows: foundRoles,
+      rows: insertedUser,
     } = await client.query(
-      "SELECT * FROM user_roles WHERE role_name=ANY($1::text[])",
-      [userRoles]
+      'INSERT INTO "User" (first_name, last_name, email, password_hash, phone, birth_date, role, registration_date, is_active) VALUES ($1, $2, $3, $4, $5, $6, $7, NOW(), true) RETURNING id_user',
+      [first_name, last_name, email, bcrypt.hashSync(password, 8), phone, birth_date, userRole]
     );
-    if (foundRoles.length !== userRoles.length) {
-      const notFoundRoles = userRoles.filter(
-        (role) => !foundRoles.find((r) => r.role_name === role)
-      );
-      res
-        .status(400)
-        .send({ message: `Invalid role(s) specified: ${notFoundRoles.join(", ")}` });
-      return;
+
+    if (insertedUser) {
+      res.send({ message: "User was registered successfully!" });
+    } else {
+      res.status(500).send({ message: "Failed to register user" });
     }
-
-    // Insert new user with hashed password and role_id
-    const { rows: insertedUser } = await client.query(
-      "INSERT INTO users (name, email, password, role_id) VALUES ($1, $2, $3, $4) RETURNING id",
-      [name, email, bcrypt.hashSync(password, 8), foundRoles[0].role_id]
-    );
-    const userId = insertedUser[0].id;
-
-    res.send({ message: "User was registered successfully!" });
 
     client.release();
   } catch (error) {
@@ -88,42 +92,43 @@ const registerUser = async (req, res) => {
 };
 
 const loginUser = async (req, res) => {
-  const { name, password } = req.body;
+  const { email, password } = req.body;
 
   try {
     const client = await poolDB.connect();
 
-    // Find user with provided name
+    // Find user with provided email
     const {
-      rows: [user],
-    } = await client.query("SELECT * FROM users WHERE name=$1", [name]);
+      rows: [User],
+    } = await client.query('SELECT * FROM "User" WHERE email=$1', [email]);
 
-    if (!user) {
+    if (!User) {
       res.status(404).send({ message: "User not found" });
       return;
     }
 
     // Create a hash from provided password and check if passwords match
-    const passwordIsValid = bcrypt.compareSync(password, user.password);
+    const passwordIsValid = bcrypt.compareSync(password, User.password_hash);
     if (!passwordIsValid) {
       res.status(401).send({ message: "Invalid password" });
       return;
     }
 
-    // Get user role
-    const {
-      rows: [role],
-    } = await client.query("SELECT role_name FROM user_roles WHERE role_id=$1", [user.role_id]);
+    const role = User.role;
 
-    // Create JWT token with user ID and role ID in JWT's payload
-    const token = jwt.sign({ id: user.id, roleId: user.role_id }, process.env.JWT_SECRET, {
+    // Create JWT token with User ID and role in JWT's payload
+    const token = jwt.sign({ id: User.id_user, role: role }, config.secret, {
       expiresIn: config.tokenExpiration, // 24 hours
     });
 
+    // Insert current timestamp to last_login in User table
+    await client.query('UPDATE "User" SET last_login = NOW() WHERE id_user = $1', [User.id]);
+
     res.send({
-      name: user.name,
-      email: user.email,
-      role: role.role_name,
+      first_name: User.first_name,
+      last_name: User.last_name,
+      email: User.email,
+      role: role,
       accessToken: token,
     });
 
@@ -140,56 +145,53 @@ const updateUserPass = async (req, res) => {
   const userId = req.userId;
 
   // Verify that the old password is correct
-  const client = await poolDB.connect();
-  const { rows } = await client.query("SELECT password FROM users WHERE id=$1", [userId]);
-  const user = rows[0];
+  try {
+    const client = await poolDB.connect();
+    const { rows } = await client.query('SELECT password_hash, role FROM "User" WHERE id_user=$1', [userId]);
+    const User = rows[0];
 
-  if (!user) {
-      return res.status(401).send({ message: "Unauthorized" });
+    if (!User) {
+        return res.status(401).send({ message: "Unauthorized: Wrong old password" });
+    }
+
+    const isPasswordValid = await bcrypt.compare(oldPassword, User.password_hash);
+    if (!isPasswordValid) {
+        return res.status(401).send({ message: "Invalid old password" });
+    }
+
+    // Update the User's password
+    const hashedNewPassword = await bcrypt.hash(newPassword, 8);
+    await client.query('UPDATE "User" SET password_hash=$1 WHERE id_user=$2', [hashedNewPassword, userId]);
+
+    res.status(200).send({ message: "Password updated successfully"});
+
+    client.release();
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: error.message });
   }
-
-  const isPasswordValid = await bcrypt.compare(oldPassword, user.password);
-  if (!isPasswordValid) {
-      return res.status(401).send({ message: "Invalid old password" });
-  }
-
-  // Update the user's password
-  const hashedNewPassword = await bcrypt.hash(newPassword, 10);
-  await client.query("UPDATE users SET password=$1 WHERE id=$2", [hashedNewPassword, userId]);
-
-  res.status(200).send({ message: "Password updated successfully" });
-
-  client.release();
 };
 
 const deleteUser = async (req, res) => {
   const userId = req.params.id;
-
   const client = await poolDB.connect();
 
   try {
     // Check if user exists
-    const {
-      rowCount
-    } = await client.query('SELECT * FROM users WHERE id = $1', [userId]);
+    const { rowCount } = await client.query('SELECT * FROM "User" WHERE id_user = $1', [userId]);
 
     if (rowCount === 0) {
-      return res.status(404).send({
-        message: 'User not found'
-      });
+      res.status(404).send({ message: "User not found" });
+      return;
     }
 
     // Delete user
-    await client.query('DELETE FROM users WHERE id = $1', [userId]);
+    await client.query('DELETE FROM "User" WHERE id_user = $1', [userId]);
 
-    res.status(200).send({
-      message: 'User deleted successfully'
-    });
+    res.send({ message: "User deleted successfully" });
   } catch (err) {
     console.error(err.message);
-    res.status(500).send({
-      message: 'Internal server error'
-    });
+    res.status(500).send({ message: "Failed to delete user" });
   } finally {
     client.release();
   }
--- middleware/oleCheckJWT.js
+++ middleware/oleCheckJWT.js
@@ -3,49 +3,49 @@ const jwt = require("jsonwebtoken");
 const config = require("../config/auth.config");
 
 const Pool = require('pg').Pool
+const connectionString = process.env.DATABASE_LINK;
+
 const poolDB = new Pool({
-  user: process.env.DATABASE_USER_NAME,
-  host: process.env.DATABASE_HOST,
-  database: process.env.DATABASE_NAME,
-  password: process.env.DATABASE_PASSWORD,
-  port: process.env.DATABASE_PORT,
-})
+  connectionString,
+  schema: 'public'
+});
+
 
 const verifyToken = async (req, res, next) => {
-    let token = req.headers["x-access-token"];
-    
+  let token = req.headers["x-access-token"];
+  
 
-    if (!token) {
-        return res.status(403).send({ message: "No token provided!" });
-    }
+  if (!token) {
+      return res.status(403).send({ message: "No token provided!" });
+  }
 
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        const client = await poolDB.connect();
+  try {
+      const decoded = jwt.verify(token, config.secret, { expiresIn: config.tokenExpiration });
+      const client = await poolDB.connect();
+      // Find user with decoded ID
+      const {
+          rows: [user],
+      } = await client.query('SELECT * FROM "User" WHERE id_user=$1', [decoded.id]);
 
-        // Find user with decoded ID
-        const {
-            rows: [user],
-        } = await client.query("SELECT * FROM users WHERE id=$1", [decoded.id]);
+      if (!user) {
+          return res.status(401).send({ message: "Unauthorized1" });
+      }
 
-        if (!user) {
-            return res.status(401).send({ message: "Unauthorized" });
-        }
+      // Store decoded JWT payload to be accessible to other middleware
+      req.userId = decoded.id;
+      req.role = decoded.role;
 
-        //store decoded JWT payload to be accessible to other middleware
-        req.userId = decoded.id;
-        req.roleId = decoded.roleId;
-        next();
+      next();
 
-        client.release();
-    } catch (err) {
-        if (err instanceof jwt.TokenExpiredError) {
-            return res.status(401).send({ message: "Unauthorized! Access token was expired!" + err.message });
-        }
-        console.error(err.message);
+      client.release();
+  } catch (err) {
+      if (err instanceof jwt.TokenExpiredError) {
+          return res.status(401).send({ message: "Unauthorized! Access token was expired!" + err.message });
+      }
+      console.error(err.message);
 
-        res.status(401).send({ message: "Unauthorized" });
-    }
+      res.status(401).send({ message: "Unauthorized2" });
+  }
 };
 
 const isAdmin = async (req, res, next) => {
@@ -53,26 +53,19 @@ const isAdmin = async (req, res, next) => {
   const client = await poolDB.connect();
 
   try {
-    const {
-      rows: [userRole],
-    } = await client.query(
-      "SELECT role_name FROM user_roles WHERE role_id = (SELECT role_id FROM users WHERE id = $1)",
-      [userId]
-    );
-
-    if (userRole.role_name !== "admin") {
-      throw new Error("User isn't admin");
+    const { rows } = await client.query('SELECT role FROM "User" WHERE id_user = $1', [userId]);
+    const userRole = rows[0].role;
+    if (userRole !== "admin") {
+      throw new Error("User is not an admin");
     }
     // user is admin
     next();
   } catch (err) {
-    res.status(401).send({ message: "Unauthorized" });
+    res.status(401).send({ message: "Unauthorized: Not an admin" });
   } finally {
     client.release();
   }
 };
-
-
 
 const oleCheckJWT = {
     verifyToken,
