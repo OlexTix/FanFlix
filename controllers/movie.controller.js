1cea255c3da210d7067b0147b3a06dd0cd461fce
DiFortis <difortis3@gmail.com>
2023-05-03 14:33:59 +0200

    changed way of connecting to database

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -1,11 +1,23 @@
-require("dotenv").config();
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
+require('dotenv').config();
+var jwt = require("jsonwebtoken");
+var bcrypt = require("bcryptjs");
 const config = require("../config/auth.config");
 const { oleCheckJWT } = require("../middleware");
-const { Pool } = require("pg");
 
-const connectionString = process.env.DATABASE_LINK;
+const Pool = require('pg').Pool;
+
+// Read variables from .env file
+const DATABASE_USER_NAME = process.env.DATABASE_USER_NAME;
+const DATABASE_HOST_NAME = process.env.DATABASE_HOST_NAME;
+const DATABASE_NAME = process.env.DATABASE_NAME;
+const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD;
+const DATABASE_PORT = process.env.DATABASE_PORT;
+
+// Create DATABASE_LINK using variables from .env file
+const DATABASE_LINK = `postgres://${DATABASE_USER_NAME}:${DATABASE_PASSWORD}@${DATABASE_HOST_NAME}:${DATABASE_PORT}/${DATABASE_NAME}?options=-c search_path=public`;
+
+const connectionString = DATABASE_LINK;
+
 const poolDB = new Pool({
   connectionString,
 });
