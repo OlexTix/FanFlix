bd47a95945a9d6d50a1f4482a9609aefa78e73de
OlexTix <olexix@gmail.com>
2023-05-07 19:18:18 +0200

    Small adjustments. Added deletion function, to do- delete cinema in AdminCinemaView

--- src/views/admin-views/AdminCinemaView.vue
+++ src/views/admin-views/AdminCinemaView.vue
@@ -2,42 +2,42 @@
     <AdminPanelTemplate>
         <p>Cinemas</p>
         <p v-if="errorMessage">{{ errorMessage }}</p>
-    <table>
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Nazwa</th>
-          <th>Adres</th>
-          <th>Nr tel</th>
-          <th>Ulica</th>
-          <th>Nr budynku</th>
-          <th>Nr lokalu</th>
-          <th>Kod pocztowy</th>
-          <th>Miejscowość</th>
-          <th>Kraj</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="cinema in cinemas" :key="cinema.id">
-          <td>{{cinema.id_cinema }}</td>
-          <td>{{cinema.name }}</td>
-          <td>{{cinema.id_address}}</td>
-          <td>{{cinema.phone}}</td>
-          <td>{{cinema.street}}</td>
-          <td>{{cinema.building_number}}</td>
-          <td>{{cinema.apartment_number}}</td>
-          <td>{{cinema.postal_code}}</td>
-          <td>{{cinema.city}}</td>
-          <td>{{cinema.country}}</td>
-          <td>
-            
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nazwa</th>
+                    <th>Adres</th>
+                    <th>Nr tel</th>
+                    <th>Ulica</th>
+                    <th>Nr budynku</th>
+                    <th>Nr lokalu</th>
+                    <th>Kod pocztowy</th>
+                    <th>Miejscowość</th>
+                    <th>Kraj</th>
+                    <th>Czynności</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="cinema in cinemas" :key="cinema.id">
+                    <td>{{ cinema.id_cinema }}</td>
+                    <td>{{ cinema.name }}</td>
+                    <td>{{ cinema.id_address }}</td>
+                    <td>{{ cinema.phone }}</td>
+                    <td>{{ cinema.street }}</td>
+                    <td>{{ cinema.building_number }}</td>
+                    <td>{{ cinema.apartment_number }}</td>
+                    <td>{{ cinema.postal_code }}</td>
+                    <td>{{ cinema.city }}</td>
+                    <td>{{ cinema.country }}</td>
+                    <td>
+                        <button class="delete-button" @click="deleteCinema(cinema.id_cinema)">Usuń</button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+
     </AdminPanelTemplate>
-    
 </template>
 
 <script>
@@ -50,69 +50,86 @@ export default {
         AdminPanelTemplate
     },
     data() {
-    return {
-      cinemas: [],
-      errorMessage: '',
-    };
-  },
-  async created() {
-    try {
-      const response = await axiosInstance.get('/api/cinemas');
-      this.cinemas = response.data;
-    } catch (error) {
-      console.error(error);
-    }
-  },
- 
-    
+        return {
+            cinemas: [],
+            errorMessage: '',
+        };
+    },
+    async created() {
+        try {
+            const response = await axiosInstance.get('/api/cinemas');
+            this.cinemas = response.data;
+        } catch (error) {
+            console.error(error);
+        }
+    },
+    methods: {
+        async deleteCinema(cinemaId) {
+            try {
+                await axiosInstance.delete(`/api/cinemas/${cinemaId}`);
+                const response = await axiosInstance.get('/api/cinemas');
+                this.cinemas = response.data;
+            } catch (error) {
+                console.error(error);
+            }
+        },
+    },
+
+
 }
 </script>
 <style>
 table {
-  width: 100%;
-  border-collapse: collapse;
-  margin: 1rem 0;
+    width: 100%;
+    border-collapse: collapse;
+    margin-bottom: 1rem;
 }
 
-th,
-td {
-  padding: 0.75rem;
-  text-align: left;
-  border: 1px solid #ccc;
+td,
+th {
+    padding: 0.5rem;
+    text-align: left;
+    border: 1px solid #ddd;
 }
+
 
 th {
-  font-weight: bold;
-  background-color: #f1f1f1;
+    font-weight: bold;
 }
 
-.button {
-  background-color: #4caf50;
-  border: none;
-  color: white;
-  padding: 0.5rem 1rem;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 1rem;
-  margin: 0.5rem 0;
-  cursor: pointer;
-  border-radius: 4px;
-}
+.delete-button {
+    border-radius: 6px;
+    background-image: linear-gradient(to bottom, #a80000, #5b0101);
+    border-color: #650a0a;
+    font-weight: 700;
+    font-size: 18px;
+    color: #ffffff;
+    margin-top: 20px;
+    display: flex;
+    justify-content: center;
+    cursor: pointer;
+  }
+
+  .delete-button:hover {
+    background-image: linear-gradient(to bottom, #420505, #550404);
+    border-color: #4a0707;
+  }
+
 
 .header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 1rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 1rem;
 }
 
 .header h1 {
-  margin: 0;
-  font-size: 2rem;
+    margin: 0;
+    font-size: 2rem;
 }
 
 .header .button-container {
-  display: flex;
-  align-items: center;
-}</style>
+    display: flex;
+    align-items: center;
+}
+</style>
