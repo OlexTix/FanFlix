b268574329e0ccedc20ec836ee0cf8cc4b242061
OlexTix <olexix@gmail.com>
2023-05-28 03:46:27 +0200

    Connected screenings and movies counters in AdminMainView. TODO- sold tickets

--- src/views/admin-views/AdminMainView.vue
+++ src/views/admin-views/AdminMainView.vue
@@ -1,33 +1,55 @@
 <template>
-    <AdminPanelTemplate>
-        <p class="title-dashboard">Panel Główny</p>
-        <div class="box-bar">
-        <div class="bar-status">
-            <span class="tile-text-top">Sprzedane bilety</span><br>
-            <img src="../../assets/ticket.svg" alt="Ikona 1" class="tile-icon">
-            <span class="tile-number">654</span>
-        </div>
-        <div class="bar-status">
-            <span class="tile-text-top">Liczba seansów</span>
-            <img src="../../assets/screening.svg" alt="Ikona 2" class="tile-icon">
-            <span class="tile-number">21</span>
-        </div>
-        <div class="bar-status">
-            <span class="tile-text-top">Liczba filmow</span>
-            <img src="../../assets/admin-assets/movies.svg" alt="Ikona 3" class="tile-icon">
-            <span class="tile-number">32</span>
-        </div>
-        </div>
-    </AdminPanelTemplate>
-    
+  <AdminPanelTemplate>
+    <p class="title-dashboard">Panel Główny</p>
+    <div class="box-bar">
+      <div class="bar-status">
+        <span class="tile-text-top">Sprzedane bilety</span><br>
+        <img src="../../assets/ticket.svg" alt="Ikona 1" class="tile-icon">
+        <span class="tile-number">{{ soldTickets }}</span>
+      </div>
+      <div class="bar-status">
+        <span class="tile-text-top">Liczba seansów</span>
+        <img src="../../assets/screening.svg" alt="Ikona 2" class="tile-icon">
+        <span class="tile-number">{{ numberOfScreenings }}</span>
+      </div>
+      <div class="bar-status">
+        <span class="tile-text-top">Liczba filmow</span>
+        <img src="../../assets/admin-assets/movies.svg" alt="Ikona 3" class="tile-icon">
+        <span class="tile-number">{{ numberOfMovies }}</span>
+      </div>
+    </div>
+  </AdminPanelTemplate>
 </template>
 
 <script>
 import AdminPanelTemplate from '../../components/templates/AdminPanelTemplate.vue';
+import axiosInstance from '../../service/apiService';
 export default {
-    components: {
-        AdminPanelTemplate
+  components: {
+    AdminPanelTemplate
+  },
+  data() {
+    return {
+      soldTickets: 1034,
+      numberOfScreenings: 0,
+      numberOfMovies: 0
+    };
+  },
+  async created() {
+    try {
+      const screeningsResponse = await axiosInstance.get('/api/panel/screenings');
+      this.screenings = screeningsResponse.data;
+      this.numberOfScreenings = this.screenings.length;
+
+      const moviesResponse = await axiosInstance.get('/api/panel/movies');
+      this.movies = moviesResponse.data;
+      this.numberOfMovies = this.movies.length;
+    } catch (error) {
+      console.error(error);
     }
+  },
+
+
 }
 </script>
 
@@ -38,17 +60,18 @@ export default {
   color: var(--color-heading);
   margin-bottom: 0.5rem;
 }
+
 .box-bar {
   display: flex;
-  justify-content: flex-start; 
+  justify-content: flex-start;
   gap: 1.2rem;
 }
 
 .bar-status {
   flex: 1;
-  max-width: 17.5rem; 
-  height: 7rem; 
-  background-color: var(--color-background);  
+  max-width: 17.5rem;
+  height: 7rem;
+  background-color: var(--color-background);
   border-radius: .6rem;
   display: flex;
   align-items: center;
@@ -57,32 +80,33 @@ export default {
 }
 
 .tile-text-top {
-  font-size: 1rem; 
-  font-weight: bold; 
-  margin-bottom: auto; 
+  font-size: 1rem;
+  font-weight: bold;
+  margin-bottom: auto;
 }
 
-.tile-icon { 
-  height: 2rem; 
-  margin-right: 0.5rem; 
+.tile-icon {
+  height: 2rem;
+  margin-right: 0.5rem;
 }
 
 .tile-number {
-  position: absolute; 
-  top: 1.5rem; 
-  right: 1.5rem; 
+  position: absolute;
+  top: 1.5rem;
+  right: 1.5rem;
 }
 
 @media screen and (max-width: 892px) {
   .box-bar {
-    flex-direction: column; 
-    align-items: flex-start; 
+    flex-direction: column;
+    align-items: flex-start;
   }
+
   .bar-status {
     flex: 1;
-    max-width: 16rem; 
-    height: 6rem; 
-    background-color: var(--color-background); 
+    max-width: 16rem;
+    height: 6rem;
+    background-color: var(--color-background);
     border-radius: 10px;
   }
 }
